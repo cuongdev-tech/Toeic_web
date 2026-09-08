af0e013d86eec1d8b11555794e0407c662583736
cuongsaidepchieu1 <truongcuong2109@gmail.com>
2026-09-08 13:30:06 +0700

    chore: bo dist va FE/dist khoi git, build lai tu source

--- dist/routes/dashboard.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const dashboard_controller_1 = require("../controllers/dashboard.controller");
-const auth_middleware_1 = require("../middlewares/auth.middleware");
-const router = (0, express_1.Router)();
-router.get('/student', auth_middleware_1.verifyToken, dashboard_controller_1.DashboardController.getStudentDashboard);
-router.get('/', auth_middleware_1.verifyToken, auth_middleware_1.isAdmin, dashboard_controller_1.DashboardController.getDashboardStats);
-exports.default = router;
